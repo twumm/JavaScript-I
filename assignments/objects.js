bf5240e449350b7172a36de8ea2e89ccf44c6b75
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-06 20:55:44 +0000

    chore: Complete challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -44,11 +44,12 @@ console.log(antonietta.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-
-// console.log(kennan.speak());
+kennan.speak = () => "Hello, my name is Kennan!";
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums = (x,y) => x * y;
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
