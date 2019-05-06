48b4f232d085245d5926986ab9f605973a434738
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-06 18:38:04 +0000

    chore: Format document

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -7,14 +7,14 @@ let anotherFunction = (param) => param;
 anotherFunction("Example");
 
 let add = (param1, param2) => param1 + param2;
-add(1,2);
+add(1, 2);
 
 let subtract = (param1, param2) => param1 - param2;
-subtract(1,2);
+subtract(1, 2);
 
 
 // Stretch
 
-exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 const triple = exampleArray.map((num) => num * 3);
 console.log(triple);
