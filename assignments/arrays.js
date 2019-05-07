938fe5c3472698d967c582395fbeea326fa8bf0f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-07 08:38:27 +0000

    refactore: Modify challenge 5 to follow order of the question

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,7 +104,7 @@ for (let i = 0; i < carYears.length; i++) {
     oldCars.push(carYears[i])
   }
 }
-console.log(oldCars.length);
+console.log(`Cars older than year 2000 - ${carYears.length - oldCars.length}`);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
